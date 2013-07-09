1df5ca9f11e87303209baae1b07cf38dd91c1d34
Gergely Imreh <imrehg@gmail.com>
2013-07-09 21:45:51 +0800

    Read text from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
-var express = require("express");
+var express = require("express"),
+    fs = require('fs');
 var app = express();
 app.use(express.logger());
 
+var index = fs.readFileSync('index.html');
+var indexHTML = index.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(indexHTML);
 });
 
 var port = process.env.PORT || 5000;
